b499f2b24133c0be94f710e63ebc9ef3c071121f
pryalkin <pppryalkin@gmail.com>
2023-05-23 02:10:25 +0300

    first commit

---
+++ src/test/java/com/bsuir/neural_network/NeuralNetworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bsuir.neural_network;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeuralNetworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
